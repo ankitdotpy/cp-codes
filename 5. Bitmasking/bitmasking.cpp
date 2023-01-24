9e72e69fbb59619a8cf976290f9baab9f9801f20
ankitd <dhimanankit262@gmail.com>
2023-01-24 12:42:23 +0530

    bitmasking codes

--- 5. Bitmasking/bitmasking.cpp
+++ 5. Bitmasking/bitmasking.cpp
@@ -24,8 +24,39 @@ int setIthBit(int num, int i){
 	return num|mask;
 }
 
-int updateIthBit(int num, int i, int upd){
-	
+int updateIthBit(int num, int i){
+	int mask = 1<<i;
+	if(num&mask)
+		return clearIthBit(num,i);
+	return setIthBit(num,i);
+
+}
+
+int clearLastIBits(int num, int i){
+	int mask = (~0)<<i; //(11111...)<<i
+	return num&mask;
+}
+
+int clearRangeOfBits(int num, int i, int j){
+	//mask? //11111...000000...11111
+	int a = (~0)<<(j+1);
+	int b = (1<<i)-1;
+	int mask = a|b;
+
+	return num&mask;
+
+}
+
+int replaceBits(int n, int m, int i, int j){
+	int temp = clearRangeOfBits(n,i,j);
+	int mask = m<<i;
+	// bitset<16> mb(mask);
+	// cout<<mb<<endl;
+	return temp|mask;
+}
+
+bool isPowerOfTwo(int num){
+	return (num&(num-1))==0;
 }
 
 signed main(){
@@ -35,6 +66,14 @@ signed main(){
 	cout<<getIthBit(43,3)<<endl;
 	cout<<clearIthBit(43,3)<<endl; // 43 = 101011
 	cout<<setIthBit(43,2)<<endl;
+	cout<<updateIthBit(43,4)<<endl;
+	cout<<clearLastIBits(43,3)<<endl;
+	cout<<clearRangeOfBits(43,1,4)<<endl;
+	cout<<replaceBits(43,10,1,4)<<endl;
 
+	// bitset<16> x(43);
+	// bitset<16> y(53);
+	// cout<<x<<endl;
+	// cout<<y<<endl;
 	return 0;
 }
